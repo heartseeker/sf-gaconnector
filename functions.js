5e6e402fa59e3c487ab1ddb81e3a54b62459e3b4
Alex Amistad <amal@erni.ph>
2019-07-19 22:20:54 +0800

    update

--- functions.js
+++ functions.js
@@ -3,7 +3,7 @@ const conn = new jsforce.Connection();
 
 module.exports = {
     connect: (callback) => {
-        conn.login(process.env.USERNAME, process.env.PASSWORD, function(err, response) {
+        conn.login('alexinformationtech@gmail.com', '@Salesforce123456zqfYryc9mvmB12WY9CLarKCuf', function(err, response) {
             if (err) {
                 return callback(err);
             }
